16addf8575e83e390d8d97ecdabb9d3eeecde94b
Dave <docbonson@gmail.com>
2023-12-07 18:25:51 -0600

    wild fire app

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import spinner from './fire-loader.gif'
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={spinner} alt="Loading..." />
+      <h1>Fetching Data...</h1>
+    </div>
+  )
+}
+
+export default Loader
