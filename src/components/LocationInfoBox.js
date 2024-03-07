c98da448646c18e941e7312bd371fc33051a740f
Dave <docbonson@gmail.com>
2024-03-07 11:24:31 -0600

    added jsdoc / removed console.log

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,3 +1,9 @@
+/**
+ * Location information component to display event details.
+ * @param {Object} props.info - The information about the event.
+ * @param {number} props.info.id - The ID of the event.
+ * @param {string} props.info.title - The title of the event.
+ */
 const LocationInfoBox = ({ info }) => {
   return (
     <div className="location-info">
--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,7 +1,7 @@
 import { Icon } from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 
-const LocationMarker = ({ lat, lng, onClick }) => {
+const LocationMarker = ({ onClick }) => {
   return (
     <div className="location-marker" onClick={onClick}>
       <Icon icon={locationIcon} className="location-icon" />
